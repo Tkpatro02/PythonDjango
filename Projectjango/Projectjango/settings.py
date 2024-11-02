4a19c5b9a5e8a1e58c6b6b3a4517777f9b48d63f
triyank.patro <triyank.patro@gmail.com>
2024-11-02 12:11:17 +0530

    newDjango

--- Projectjango/Projectjango/settings.py
+++ Projectjango/Projectjango/settings.py
@@ -37,8 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'chai',
+    'tailwind',
+    'theme',
+    'django_browser_reload',
+   
 ]
-
+TAILWIND_APP_NAME='theme'
+INTERNAL_IPS=['127.0.0.1']
+NPM_BIN_PATH = "C:/Program Files/nodejs/npm.cmd"  # Adjust this path if needed
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -47,7 +54,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django_browser_reload.middleware.BrowserReloadMiddleware',
+
 ]
+
 
 ROOT_URLCONF = 'Projectjango.urls'
 
